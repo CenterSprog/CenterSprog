6e8c3de7f7abf4ce1dbb1501743f3def790ee048
Damian Trafiałek <damian.trafialek@gmail.com>
2023-11-27 09:48:53 +0100

    add: auth dependencies

--- Presenation_Logic_Tier/WebAPI/Program.cs
+++ Presenation_Logic_Tier/WebAPI/Program.cs
@@ -4,6 +4,9 @@ using Application.DAOInterfaces;
 using Application.gRPCClients;
 using Application.Logic;
 using Application.LogicInterfaces;
+using Domain.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +20,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHeartbeatLogic, HeartbeatLogic>();
 builder.Services.AddScoped<IHeartbeatDAO, HeartbeatClient>();
 builder.Services.AddScoped<IAuthClient,AuthClient>();
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+AuthorizationPolicies.AddPolicies(builder.Services);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
