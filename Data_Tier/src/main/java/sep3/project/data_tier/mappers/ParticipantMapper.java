c76ce056b6e26d07b4c0f920e3d417d899c4bb2f
Damian Trafiałek <damian.trafialek@gmail.com>
2023-12-01 20:16:09 +0100

    fix: protofiles imports

--- Data_Tier/src/main/java/sep3/project/data_tier/mappers/ParticipantMapper.java
+++ Data_Tier/src/main/java/sep3/project/data_tier/mappers/ParticipantMapper.java
@@ -2,9 +2,7 @@ package sep3.project.data_tier.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import sep3.project.data_tier.entity.LessonEntity;
 import sep3.project.data_tier.entity.UserEntity;
-import sep3.project.protobuf.Lesson;
 import sep3.project.protobuf.UserParticipant;
 
 @Mapper
